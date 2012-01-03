5057d06feb39262a3675e5160b5d89d0ed0d3be2
Rogerio Prado de Jesus <c066770@mail.caixa>
2012-01-03 19:58:30 -0200

    Commit inicial
    Signed-off-by: Rogerio Prado de Jesus <c066770@mail.caixa>

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php
+require 'vendor/.composer/autoload.php';
+
+$app = new Silex\Application(); 
+$app['debug'] = true;
+$app->get('/', function() use ($app) {
+    
+});
+
+$app->get('/hello/{name}', function($name) use($app) { 
+    return 'Hello '.$app->escape($name); 
+}); 
+
+$blogPosts = array(
+    1 => array(
+        'date'      => '2011-03-29',
+        'author'    => 'igorw',
+        'title'     => 'Using Silex',
+        'body'      => '...',
+    ),
+);
+
+$app->get('/blog', function () use ($blogPosts) {
+    $output = '';
+    foreach ($blogPosts as $post) {
+        $output .= $post['title'];
+        $output .= '<br />';
+    }
+
+    return $output;
+});
+
+$app->get('/blog/show/{id}', function (Silex\Application $app, $id) use ($blogPosts) {
+    if (!isset($blogPosts[$id])) {
+        $app->abort(404, "Post $id does not exist.");
+    }
+
+    $post = $blogPosts[$id];
+
+    return  "<h1>{$post['title']}</h1>".
+            "<p>{$post['body']}</p>";
+});
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$app->post('/feedback', function (Request $request) {
+    $message = $request->get('message');
+    mail('feedback@yoursite.com', '[YourSite] Feedback', $message);
+
+    return new Response('Thank you for your feedback!', 201);
+});
+
+$app->error(function (\Exception $e, $code) {
+    switch ($code) {
+        case 404:
+            $message = 'The requested page could not be found.';
+            break;
+        default:
+            $message = 'We are sorry, but something went terribly wrong.';
+    }
+
+    return new Response($message, $code);
+});
+
+$app->run(); 
